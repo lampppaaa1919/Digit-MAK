dd29f50248fe5990bf2f59fb0f8dbe02bd67b5d2
lampppaaa1919 <filiptashkov1@gmail.com>
2026-08-08 16:12:06 +0200

    Vercel bootstrap added

--- backend/src/DigitMak.Portal.Api/Program.cs
+++ backend/src/DigitMak.Portal.Api/Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddCors(options =>
                 "http://127.0.0.1:5173",
                 "http://localhost:3000",
                 "http://127.0.0.1:3000",
-		"https://digit-c19g40o69-l-85da.vercel.app",
+		"https://digit-c19g40o69-l-85da.vercel.app"
             )
             .AllowAnyHeader()
             .AllowAnyMethod()
